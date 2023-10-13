be57ddcbf8b1da1aa4ca7925025969f10fe130a6
Darryl Greig <darryl.greig@owerya.com>
2023-10-13 19:29:18 +0100

    updated solutions to latest version

--- labs/solutions/5-terraform-config-language-i/learn-terraform-count-foreach/README.md
+++
@@ -1,3 +0,0 @@
-# Learn Terraform count and for_each
-
-Learn what Terraform count and for_each are and when to use them.
--- labs/solutions/5-terraform-config-language-i/learn-terraform-resources/main.tf
+++ labs/solutions/5-terraform-config-language-i/learn-terraform-resources/main.tf
@@ -1,3 +1,6 @@
+# Copyright (c) HashiCorp, Inc.
+# SPDX-License-Identifier: MPL-2.0
+
 provider "aws" {
   region = "us-west-2"
 }
@@ -10,7 +13,6 @@ resource "aws_instance" "web" {
   ami           = "ami-a0cfeed8"
   instance_type = "t2.micro"
   user_data     = file("init-script.sh")
-
   vpc_security_group_ids = [aws_security_group.web-sg.id]
 
   tags = {
@@ -21,17 +23,16 @@ resource "aws_instance" "web" {
 resource "aws_security_group" "web-sg" {
   name = "${random_pet.name.id}-sg"
   ingress {
-    from_port = 80
-    to_port = 80
-    protocol = "tcp"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
-
   }
-  
+
   egress {
-    from_port        = 0
-    to_port          = 0
-    protocol         = "-1"
-    cidr_blocks      = ["0.0.0.0/0"]
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
   }
 }
